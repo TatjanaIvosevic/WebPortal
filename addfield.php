85ffb5464643797d320e478c00aa1a36d1d14551
TatjanaIvosevic <tatjana.ivosevic1@gmail.com>
2019-12-11 08:59:35 +0100

    fields

--- addfield.php
+++ addfield.php
@@ -1,4 +1,4 @@
-<?php require "db-config.php"; ?>
+<?php require "db_config.php"; ?>
 <?php include_once "includes/header.php"; ?>
 <body>
 <?php include_once "includes/navbar.php"; ?>
@@ -11,7 +11,40 @@
                     <div class="row">
                         <div class="col-sm-12 text-center mt-md-2">
 
+                            <form method="POST" enctype="multipart/form-data">
+                                <input type="file" name="image">
+                                <input type="submit" name="submit" value="upload">
+                            </form>
 
+                            <?php
+                                if(isset($_POST['submit'])){
+                                    if(getimagesize($_FILES['image']['tmp_name'])==FALSE)
+                                    {
+                                        echo " error ";
+                                    }
+                                    else
+                                    {
+                                        $image = $_FILES['image']['tmp_name'];
+                                        $image = addslashes(file_get_contents($image));
+                                        saveimage($image);
+                                    }
+                                }
+                                function saveimage($image)
+                                {
+                                    global $conn;
+                                    $qeury="insert into field (image) values ('$image')";
+                                    $result=mysqli_query( $conn,$qeury);
+                                    if($result)
+                                    {
+                                        echo " <br/>Image uploaded.";
+                                        header('location:WebPortal/index.php');
+                                    }
+                                    else
+                                    {
+                                        echo " error ";
+                                    }
+                                }
+                            ?>
                         </div>
                     </div>
                 </div>
@@ -21,4 +54,3 @@
 </div>
 <?php include_once "includes/footer.php"; ?>
 </body>
-
--- addfieldregistration.php
+++ addfieldregistration.php
@@ -10,8 +10,12 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col-sm-12 text-center mt-md-2">
-
-
+                            <h2>Da li ste registrovani?</h2>
+                            <p>Samo registrovani korisnici smeju da dodaju terene.</p>
+                            <p>Zato popuni jednostavnu formu i uživaj u pogodnostima, koje ti nudimo kao registrovanom korisniku.</p>
+                            <button class="btn btn-primary">Registruj se</button>
+                            <p>Stari korisnik, koji ima svoj profil kod nas?</p>
+                             <button class="btn btn-secondary">Uloguj se</button>
                         </div>
                     </div>
                 </div>
@@ -21,3 +25,4 @@
 </div>
 <?php include_once "includes/footer.php"; ?>
 </body>
+
