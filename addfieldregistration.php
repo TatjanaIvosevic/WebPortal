410d03226917848bd74c2dc4385d5fa2ca863c28
TatjanaIvosevic <tatjana.ivosevic1@gmail.com>
2019-12-14 23:14:59 +0100

    add

--- addfieldregistration.php
+++ addfieldregistration.php
@@ -23,22 +23,36 @@
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h5 class="modal-title" id="registrationModalLabel">Registracija</h5>
+                                            <h5 class="modal-title" id="registrationModalLabel">Uloguj se</h5>
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                 <span aria-hidden="true">&times;</span>
                                             </button>
                                         </div>
                                         <div class="modal-body">
-                                            <form>
-                                                <input type="text" name="name" value="Upisi svoje ime">
-                                                <input type="text" name="surname" value="Upisi svoje prezime">
-                                                <input type="text" name="username" value="Upisi ime koje ces koristiti na sajtu">
-                                                <input type="password" name="password" value="Upisi svoje lozinku">
+                                            <form class="px-4 py-3">
+                                                <div class="form-group">
+                                                    <label for="exampleDropdownFormEmail1">Email address</label>
+                                                    <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleDropdownFormPassword1">Password</label>
+                                                    <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password">
+                                                </div>
+                                                <div class="form-group">
+                                                    <div class="form-check">
+                                                        <input type="checkbox" class="form-check-input" id="dropdownCheck">
+                                                        <label class="form-check-label" for="dropdownCheck">
+                                                            Remember me
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                                <button type="submit" class="btn btn-primary">Sign in</button>
                                             </form>
+                                            <div class="dropdown-divider"></div>
                                         </div>
                                         <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Otkaži</button>
-                                            <button type="button" class="btn btn-primary">Pošalji</button>
+                                            <a class="dropdown-item" href="#">New around here? Sign up</a>
+                                            <a class="dropdown-item" href="#">Forgot password?</a>
                                         </div>
                                     </div>
                                 </div>
@@ -54,5 +68,5 @@
 </body>
 
 <script>
-    $('#myModal').modal('show');
+    $('#registrationModal').modal('show');
 </script>
